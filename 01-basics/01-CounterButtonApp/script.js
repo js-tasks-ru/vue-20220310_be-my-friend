10f1a24e3a74a0dd6c5d4ec2d21bde4c203b32a7
Oxana Pfeifer <be-my-friend@mail.ru>
2022-03-22 01:48:46 +0100

    2nd task

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,12 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
-
-// Создайте Vue приложение
+import { createApp,defineComponent } from './vendor/vue.esm-browser.js';
+const Root = defineComponent({
+    name: 'Root',
+    data(){
+        return{
+            counter: 0,
+        };
+    },
+});
+const app = createApp(Root);
+const vm = app.mount('#app');
+window.vm = vm;
